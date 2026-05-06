2b838b952573adf31e03249031f2b7f87e3d6395
Nusrat Sharmin Era <86783061+nusratera@users.noreply.github.com>
2026-05-06 13:20:29 +0600

    Update Nusrat_760.sql

--- Nusrat_760.sql
+++ Nusrat_760.sql
@@ -20,31 +20,3 @@ CREATE TABLE appointment (
         ON DELETE CASCADE
 );
 
-CREATE TABLE treatment (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-
-    appointment_id BIGINT UNIQUE,
-
-    description TEXT,
-    cost DECIMAL(10,2),
-
-    CONSTRAINT fk_appointment
-        FOREIGN KEY (appointment_id)
-        REFERENCES appointment(id)
-    ON DELETE CASCADE
-);
-
-CREATE TABLE patient (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    age INT,
-    gender VARCHAR(10),
-    phone VARCHAR(20)
-);
-
-CREATE TABLE doctor (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    specialization VARCHAR(100),
-    phone VARCHAR(20)
-);
